e9bda3787be3921855a27657b134442672e3d750
nikhilswagdas <38124918+nikhilswagdas@users.noreply.github.com>
2018-04-24 14:50:05 +0530

    edited template

--- limb.py
+++ limb.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import math
 
 class BodyPart:
-    time=[]
-    def __init__(self, length,mass, CoM, Inertia,distal,proximal):
+    def __init__(self, length,mass, CoM, Inertia,distal,proximal,time):
+        self.time=time
         self.sample=len(distal[0])
         self.length=length
         self.CoM=CoM
@@ -32,7 +32,7 @@ class BodyPart:
         unfil=np.zeros(self.sample)
         BodyPart.absangle(self)
         for i in range(1,self.sample-1):
-            unfil[i] = (self.angle[i+1]-self.angle[i-1])/(BodyPart.time[i+1]-BodyPart.time[i-1])
+            unfil[i] = (self.angle[i+1]-self.angle[i-1])/(self.time[i+1]-self.time[i-1])
             self.omega=filterdata(unfil,5)
         return self.omega
 
@@ -40,15 +40,15 @@ class BodyPart:
         BodyPart.omega(self)
         unfil=np.zeros(self.sample)
         for i in range(1,self.sample-1):
-            unfil[i] = (self.omega[i+1]-self.omega[i-1])/(BodyPart.time[i+1]-BodyPart.time[i-1])
+            unfil[i] = (self.omega[i+1]-self.omega[i-1])/(self.time[i+1]-self.time[i-1])
             self.alpha=filterdata(unfil,5)
         return self.alpha
 
     def velProxim(self): #j1 is joint angle dataframe #n is number of datapoints 
         unfil=[np.zeros(self.sample),np.zeros(self.sample)]
         for i in range(1,self.sample-1):
-            unfil[0][i] = (self.proximal[0][i+1]-self.proximal[0][i-1])/(BodyPart.time[i+1]-BodyPart.time[i-1])
-            unfil[1][i] = (self.proximal[1][i+1]-self.proximal[1][i-1])/(BodyPart.time[i+1]-BodyPart.time[i-1])
+            unfil[0][i] = (self.proximal[0][i+1]-self.proximal[0][i-1])/(self.time[i+1]-self.time[i-1])
+            unfil[1][i] = (self.proximal[1][i+1]-self.proximal[1][i-1])/(self.time[i+1]-self.time[i-1])
         self.velProxim[0]=filterdata(unfil[0],5)/1000
         self.velProxim[1]=filterdata(unfil[1],5)/1000
         return self.velProxim
@@ -57,8 +57,8 @@ class BodyPart:
         BodyPart.velProxim(self)
         unfil=[np.zeros(self.sample),np.zeros(self.sample)]
         for i in range(1,self.sample-1):
-            unfil[0][i] = (self.velProxim[0][i+1]-self.velProxim[0][i-1])/(BodyPart.time[i+1]-BodyPart.time[i-1])
-            unfil[1][i] = (self.velProxim[1][i+1]-self.velProxim[1][i-1])/(BodyPart.time[i+1]-BodyPart.time[i-1])
+            unfil[0][i] = (self.velProxim[0][i+1]-self.velProxim[0][i-1])/(self.time[i+1]-self.time[i-1])
+            unfil[1][i] = (self.velProxim[1][i+1]-self.velProxim[1][i-1])/(self.time[i+1]-self.time[i-1])
         self.accelProxim[0]=filterdata(unfil[0],5)
         self.accelProxim[1]=filterdata(unfil[1],5)
         return self.accelProxim
@@ -88,15 +88,7 @@ def Power(omega1,omega2,M):
     power=np.multiply(Jw,M)
     return power
     
-def aCoM(m1,a1,m2,a2,m3,a3,m4,a4):
-    acom=[np.zeros(106),np.zeros(106)]
-    F=[np.zeros(106),np.zeros(106)]
-    for i in range(0,2):
-        for j in range(0,106):
-            acom[i][j]=(m1*a1[i][j]+m2*a2[i][j]+m3*a3[i][j]+m4*a4[i][j])/(m1+m2+m3+m4)
-            F[i][j]=(m1+m2+m3+m4)*(9.81+acom[i][j])
-    return F
-
+    
 def filterdata(y,n):
     from scipy.signal import filtfilt
     b = [1.0 / n] * n
@@ -105,9 +97,9 @@ def filterdata(y,n):
     return yy;
 
 def AnthroData(weight,height):
-    data=np.zeros(15)
+    data=np.zeros(19)
     misc= (7.8*9.6*9.6*49.5)+(46.84*31.6*31.6*50.3)+2*(2.7*16.4*16.4*32.3)+2*(2.7*13.7*13.7*30.3)+2*(0.6*8.2*8.2*29.7) #sum of Icm of all the upper body parts
-    data[0]= weight*67.2/100 #weight of UB
+    data[0]= weight*68.2/100 #weight of UB
     data[1]= height*14.2/100 #height of com of UB from hip 
     data[2]= data[0]*data[1]*data[1]+misc #moment for UB
     data[3]= weight*9.9/100#weight of thigh
@@ -118,8 +110,12 @@ def AnthroData(weight,height):
     data[8]= height*23.3/100#height of shank
     data[9]= data[8]*43/100 #height of com shank
     data[10]= data[7]*data[8]*data[8]*30.2/100 #moment of shank
-    data[11]= weight*1.4/100#weight of foot
-    data[12]= height*11.7/100 #height of foot
+    data[11]= weight*(1.4-0.361)/100#weight of foot
+    data[12]= height*(11.7-3.53)/100 #height of foot
     data[13]= data[12]*50/100 #height of com foot
     data[14]= data[11]*data[12]*data[12]*47.5/100 #moment of foot
+    data[15]=weight*.361/100 #weight of toe
+    data[16]=height*3.53/100 #height of toe
+    data[17]=data[16]/2 #height of com of toe
+    data[18]=data[15]*data[16]*data[16]/12 #moment of toe
     return data
